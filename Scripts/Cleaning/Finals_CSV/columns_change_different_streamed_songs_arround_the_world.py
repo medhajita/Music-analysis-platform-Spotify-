af6f3940cb0d8fd6269834403504bb4b08370f99
medhajita <hajita.mohamed@gmail.com>
2026-03-14 23:30:13 +0100

    changing names and ordres of some columns for the most_streamed_songs.csv

--- Scripts/Cleaning/Finals_CSV/columns_change_different_streamed_songs_arround_the_world.py
+++ Scripts/Cleaning/Finals_CSV/columns_change_different_streamed_songs_arround_the_world.py
@@ -6,16 +6,15 @@ df = pd.read_csv(r"D:\ECE_WORK\Music-analysis-platform-Spotify-\Data\Finals_CSV\
 print("Colonnes actuelles :", list(df.columns))
 
 df = df.rename(columns={
-    "country": "streamed_country",
-    "pk_streams": "peak_streams",
-    "total": "total_streams_track_per_country",
+    "total_streams_track_per_country": "total_streams_song_per_country",
+    "title" : "song_title",
+    "track_id" : "song_id"
 })
 
-new_order = ["artist_spotify_id", "artist", "title", "track_id", "streamed_country", "peak_streams", "total_streams_track_per_country",]
+new_order = ["artist_spotify_id", "artist", "song_title", "song_id", "streamed_country", "peak_streams", "total_streams_song_per_country",]
 
 df = df[new_order]
 
 df.to_csv(r"D:\ECE_WORK\Music-analysis-platform-Spotify-\Data\Finals_CSV\different_streamed_songs_arround_the_world.csv", index=False)
 print("Fichier sauvegardé")
 print("Nouvel ordre :", list(df.columns))
-
