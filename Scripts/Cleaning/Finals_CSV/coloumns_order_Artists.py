98b71cfaf72759a409f5f1a070a8a5370984091c
medhajita <hajita.mohamed@gmail.com>
2026-03-14 21:38:16 +0100

    add scripts that change the order of artists csv

---
+++ Scripts/Cleaning/Finals_CSV/coloumns_order_Artists.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+df = pd.read_csv(r"D:\ECE_WORK\Music-analysis-platform-Spotify-\Data\Finals_CSV\Artists.csv")
+
+print("Colonnes actuelles :", list(df.columns))
+
+
+new_order = ["spotify_id", "artist", "image_url", "country", "language", "type", "genre",
+             "followers",  "daily_gain_followers", "weekly_gain_followers",
+             "listeners", "daily_gain_listeners", "monthly_gain_listeners", "peak_listeners", "date_peak_listeners",
+             "total_streams", "solo_streams" , "feat_streams",
+             "tracks", "1B", "100M", "10M", "1M"]
+
+df = df[new_order]
+
+df.to_csv(r"D:\ECE_WORK\Music-analysis-platform-Spotify-\Data\Finals_CSV\Artists.csv", index=False)
+print("Fichier sauvegardé")
+print("Nouvel ordre :", list(df.columns))
