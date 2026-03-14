e86d1fe8307326f0e43992b1e04429068bbb32bf
medhajita <hajita.mohamed@gmail.com>
2026-03-14 08:10:05 +0100

    add the file number 3 of cleaned csv from chartmasters

---
+++ Scripts/Cleaning/ChartMasters/chartmasters_most_streamed_albums_cleaning.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+# Charger le fichier CSV
+df = pd.read_csv(r"D:\ECE_WORK\ProjetEnglobant\Data\Scraping_Data\ChartMasters\chartmasters_most_streamed_albums.csv")
+
+# Afficher les dimensions
+print(" Dimensions du dataset :", df.shape)
+
+#Valeurs manquantes
+print("\n Valeurs manquantes par colonne :")
+print(df.isnull().sum())
+
+#Nombre total de valeurs manquantes
+print("\n Total valeurs manquantes :")
+print(df.isnull().sum().sum())
+
+#Lignes doublons
+print("\n Nombre de lignes doublons :")
+print(df.duplicated().sum())
+
+#Afficher les lignes doublons
+print("\n Lignes doublons :")
+print(df[df.duplicated()])
+
+print("\n Nombre de lignes avant nettoyage :", len(df))
+
+#Supprimer les lignes avec valeurs manquantes
+df = df.dropna()
+
+#Supprimer les doublons (garde une seule occurrence)
+df = df.drop_duplicates()
+
+print("\n Nombre de lignes apres nettoyage :", len(df))
+
+# supprimer les virgules et convertir en nombre
+df["streams_albums"] = df["streams_albums"].str.replace(",", "").astype(int)
+print("\n",df["streams_albums"].head())
+
+df["weekly_gain_streams_albums"] = df["weekly_gain_streams_albums"].str.replace(",", "").astype(int)
+print("\n",df["weekly_gain_streams_albums"].head())
+
+df["monthly_gain_streams_albums"] = df["monthly_gain_streams_albums"].str.replace(",", "").astype(int)
+print("\n",df["monthly_gain_streams_albums"].head())
+
+df["year_albums"] = df["year_albums"].str.replace(",", "").astype(int)
+print("\n",df["year_albums"].head())
+
+
+#Sauvegarder le nouveau CSV propre
+df.to_csv(r"D:\ECE_WORK\ProjetEnglobant\Data\Clean_Data\ChartMasters\chartmasters_most_streamed_albums_clean.csv", index=False)
+
+print(" \n Nettoyage terminé.")
