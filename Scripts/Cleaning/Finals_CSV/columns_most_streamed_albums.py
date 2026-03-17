41299d3a423c19a3ca0cb4d10f0d1e2227e408a3
medhajita <hajita.mohamed@gmail.com>
2026-03-17 01:50:54 +0100

    nothing special

--- Scripts/Cleaning/Finals_CSV/columns_most_streamed_albums.py
+++ Scripts/Cleaning/Finals_CSV/columns_most_streamed_albums.py
@@ -1,16 +1,35 @@
 import pandas as pd
-
+import re
 
 df = pd.read_csv(r"D:\ECE_WORK\Music-analysis-platform-Spotify-\Data\Finals_CSV\most_streamed_albums.csv")
 
 print("Colonnes actuelles :", list(df.columns))
 
+"""
 df = df.drop(columns=["rank"])
 
 df = df.rename(columns={
     "image_url": "album_image_url",
     "year_albums": "release_year_albums",
 })
+"""
+
+SPECIAL_ARTISTS = ["Tyler, The Creator", "Earth, Wind & Fire", "Grover Washington, Jr."]
+
+def extract_main_artist(name):
+    name = str(name).strip()
+
+    # Check if starts with a special artist
+    for special in SPECIAL_ARTISTS:
+        if name.startswith(special):
+            return special
+
+    # Split on ft., ft , &, or comma
+    result = re.split(r'\s+(ft\.|ft\s|&)\s+|,\s+', name, maxsplit=1)[0]
+    return result.strip()
+
+
+df['artist'] = df['artist'].apply(extract_main_artist)
 
 new_order = ["artist_spotify_id", "artist", "album_title", "album_spotify_id", "album_image_url",
              "type", "genre", "language", "release_year_albums",
--- Scripts/Cleaning/Finals_CSV/columns_most_streamed_songs.py
+++ Scripts/Cleaning/Finals_CSV/columns_most_streamed_songs.py
@@ -1,18 +1,36 @@
 import pandas as pd
-
+import re
 
 df = pd.read_csv(r"D:\ECE_WORK\Music-analysis-platform-Spotify-\Data\Finals_CSV\most_streamed_songs.csv")
 
 print("Colonnes actuelles :", list(df.columns))
 
-df = df.drop(columns=["rank"])
+# df = df.drop(columns=["rank"])
 
+"""
 df = df.rename(columns={
     "image_url": "song_image_url",
     "song": "song_title",
     "spotify_id": "artist_spotify_id",
     "year_songs": "release_year_songs",
 })
+"""
+SPECIAL_ARTISTS = ["Tyler, The Creator", "Earth, Wind & Fire", "Grover Washington, Jr."]
+
+def extract_main_artist(name):
+    name = str(name).strip()
+
+    # Check if starts with a special artist
+    for special in SPECIAL_ARTISTS:
+        if name.startswith(special):
+            return special
+
+    # Split on ft., ft , &, or comma
+    result = re.split(r'\s+(ft\.|ft\s|&)\s+|,\s+', name, maxsplit=1)[0]
+    return result.strip()
+
+
+df['artist'] = df['artist'].apply(extract_main_artist)
 
 new_order = ["artist_spotify_id", "artist", "song_title", "song_image_url",
              "release_year_songs","genre", "language",
